0161c3933e019b981b69ce0f7c217c1ae538eb6a
AbidahNabilah <abdhnabilah@gmail.com>
2021-03-22 16:56:44 +0700

    latihan

---
+++ projek_1/bin/projek_1.dart
@@ -0,0 +1,5 @@
+import 'package:projek_1/projek_1.dart' as projek_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${projek_1.calculate()}!');
+}
---
+++ projek_1/test/projek_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:projek_1/projek_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
